724d98714dda4743fe2ea14b39fe2d1812cfd42b
Vishesh <visheshsingh16@gmail.com>
2018-02-17 18:17:38 -0600

    Capitalize using map() function

--- index.js
+++ index.js
@@ -80,13 +80,22 @@ function reverseInt(int) {
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
 function capitalizeLetters(str) {
-    const strArr = str.toLowerCase().split(' ');
+    ///////////////////////////////////////
+    //METHOD 1: USING FOR LOOP
+    /* const strArr = str.toLowerCase().split(' ');
 
     for(let i = 0; i < strArr.length; i++ ){
         strArr[i] = strArr[i].substring(0,1).toUpperCase() + strArr[i].substring(1);
     }
 
-    return strArr.join(' ');
+    return strArr.join(' '); */
+
+    //////////////////////////////////////
+    //METHOD 2: USING MAP FUNCTION
+    return str.toLowerCase().split(' ')
+        .map(function(word) {
+            return word[0].toUpperCase() + word.substr(1);
+        }).join(' ');
 }
 
 
