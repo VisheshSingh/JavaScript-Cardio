53bcd421a2b5bbb414033812ec84fb5cee8dbe77
Vishesh <visheshsingh16@gmail.com>
2018-07-01 19:53:41 -0500

    performance increased by double

--- isPrime.js
+++ isPrime.js
@@ -6,16 +6,22 @@ let isPrime = n => {
 
   if (n < 2 || !Number.isInteger(n) || n % 2 === 0) return false;
 
-  for (let i = 3; i < n - 1; i++) {
+  let ctr = 0;
+  for (let i = 3; i < n - 1; i += 2) {
+    ctr++;
     if (n % i === 0) {
+      console.log("ctr:", ctr);
       return false;
     }
   }
+  console.log("ctr:", ctr);
   return true;
 };
 
-for (let i = 0; i < 30; i++) {
-  if (isPrime(i)) {
-    console.log(i);
-  }
-}
+console.log(isPrime(101));
+
+// for (let i = 0; i < 30; i++) {
+//   if (isPrime(i)) {
+//     console.log(i);
+//   }
+// }
