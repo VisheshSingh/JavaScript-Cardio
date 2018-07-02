83341c982bbd1b98172d9953d57d3c792336a84d
Vishesh <visheshsingh16@gmail.com>
2018-07-01 19:59:21 -0500

    Algorithm improved significantly

--- isPrime.js
+++ isPrime.js
@@ -1,13 +1,25 @@
 // 0, 1 are not prime
 // 2 is prime. All the other even numbers are not prime
 
+// n = x*y;
+// => x < y
+// => x*x < x*y
+// => x*x < n
+// => x < sqrt(n)
+
+// e.g: 6 = 1*6, 2*3, 3*2, 6*1
+// => 2 < 3
+// => 2*2 < 2*3
+// => 4 < 6
+// => 2 < sqrt(6)
+
 let isPrime = n => {
   if (n === 2) return true;
 
   if (n < 2 || !Number.isInteger(n) || n % 2 === 0) return false;
 
   let ctr = 0;
-  for (let i = 3; i < n - 1; i += 2) {
+  for (let i = 3; i < Math.sqrt(n); i += 2) {
     ctr++;
     if (n % i === 0) {
       console.log("ctr:", ctr);
@@ -18,7 +30,7 @@ let isPrime = n => {
   return true;
 };
 
-console.log(isPrime(101));
+console.log(isPrime(100000001));
 
 // for (let i = 0; i < 30; i++) {
 //   if (isPrime(i)) {
