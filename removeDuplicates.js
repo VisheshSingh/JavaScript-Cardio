5ff828863ac673ec83e3e90e25d88c6ea6f3f57d
Vishesh <visheshsingh16@gmail.com>
2018-07-01 20:12:53 -0500

    remove duplicates

---
+++ removeDuplicates.js
@@ -0,0 +1,37 @@
+// SOLUTION 1
+// TIME COMPLEXITY : n*n;
+let a = [1, 2, 5, 2, 1, 8];
+
+let b = [];
+
+let len = a.length;
+for (let i = 0; i < len; i++) {
+  if (b.indexOf(a[i]) === -1) {
+    b.push(a[i]);
+  }
+}
+
+console.log(b);
+
+// SOLUTION 2
+// TIME COMPLEXITY: n*log n
+let c = [1, 2, 5, 2, 1, 8];
+
+let d = [];
+let lent = a.length;
+c.sort(); // [1,1,2,2,5,8];
+let temp;
+
+for (let i = 0; i < lent; i++) {
+  if (c[i] !== temp) {
+    d.push(c[i]);
+    temp = c[i];
+  }
+}
+
+console.log(d);
+
+// SOLUTION 3
+let x = [1, 2, 5, 2, 1, 8];
+let y = [...new Set(a)];
+console.log(y);
